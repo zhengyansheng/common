1497d8651e2feb747547ed80e985f0273eadebf7
zhengyansheng <zhengyansheng@huohua.cn>
2022-04-13 17:35:03 +0800

    优化 map

--- map.go
+++ map.go
@@ -10,3 +10,14 @@ func MergeJSONMaps(maps ...map[string]interface{}) (result map[string]interface{
 	}
 	return result
 }
+
+// MergeJSONMapsV2 合并多个map
+func MergeJSONMapsV2(maps ...map[string]string) (result map[string]string) {
+	result = make(map[string]string)
+	for _, m := range maps {
+		for k, v := range m {
+			result[k] = v
+		}
+	}
+	return result
+}
