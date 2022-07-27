3a4f02a59d78f1fdef121d3811ed959b2e8f1ccf
zhengyansheng <zhengyansheng@huohua.cn>
2022-07-27 11:50:02 +0800

    fix copy

--- copy.go
+++ copy.go
@@ -3,5 +3,5 @@ package common
 import "github.com/jinzhu/copier"
 
 func CopyWithOption(toValue interface{}, fromValue interface{}) error {
-	return copier.CopyWithOption(&toValue, &fromValue, copier.Option{IgnoreEmpty: true, DeepCopy: true})
+	return copier.CopyWithOption(toValue, fromValue, copier.Option{IgnoreEmpty: true, DeepCopy: true})
 }
