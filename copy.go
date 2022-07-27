571654833016b65abcd1fa128397667daf86a28f
zhengyansheng <zhengyansheng@huohua.cn>
2022-07-27 11:44:00 +0800

    add copy

---
+++ copy.go
@@ -0,0 +1,7 @@
+package common
+
+import "github.com/jinzhu/copier"
+
+func CopyWithOption(toValue interface{}, fromValue interface{}) error {
+	return copier.CopyWithOption(&toValue, &fromValue, copier.Option{IgnoreEmpty: true, DeepCopy: true})
+}
